db76bc607813396d8ad12413304acd79dc1b997b
Chileus <n.van.maanen@st.hanze.nl>
2016-02-12 01:41:13 +0100

    AngularJS added
    New PHP call too database in data.php

--- data.php
+++ data.php
@@ -1,26 +1,27 @@
 <?php
         
 		$servername = "localhost";
-		$databasename= "frameworks/extscripting";
+		$databasename= "db_frameworks-extscripting";
 
 		// Create connection
 		$conn=mysql_connect($servername,"root","");
 		
 		mysql_select_db($databasename);
 		
-		$user = "SELECT Naam FROM person WHERE PersonID=0";
+		$user = "SELECT UserName, UserPass FROM users";
 	
 		$userresult	= mysql_query($user,$conn);
-		
 		
 		
         require_once "vo_person.inc.php";
         $person = new Person();
 		
 		while ($row = mysql_fetch_assoc($userresult)) {
-			$person->firstName=$row['Naam'];
+			$person->firstName=$row['UserName'];
 		}
         $person->lastName="doedel";
 		
-		echo json_encode($person);
+		header('Content-Type: application/json');
+		$json = json_encode($person);
+		echo $json;
  ?>
--- vo_person.inc.php
+++ vo_person.inc.php
@@ -2,16 +2,7 @@
 
 class Person
 {
-   var $personId;
    var $firstName;
-   var $insertion;
    var $lastName;
-   var $schoolId;
-   var $emailAddress;
-   var $password;  
-   var $lastLoginDate;
-   var $questions;
-   var $role;
-   var $domains;
 }
 
